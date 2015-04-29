6d7644a1ec0005ee2d41691afcb59296b1d016ce
Abe Hiroshi <abe@x-wave.co.jp>
2015-04-29 23:42:55 +0900

    fix image tag

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
-echo "docker push $IMAGE_NAME:${IMAGE_TAG-latest}"
+echo "docker push $DOCKER_REGISTRY/$DOCKER_USER/$IMAGE_NAME:${IMAGE_TAG-latest}"
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD -e $DOCKER_EMAIL $DOCKER_REGISTRY
-docker push $IMAGE_NAME:${IMAGE_TAG-latest}
+docker tag $IMAGE_NAME $DOCKER_REGISTRY/$DOCKER_USER/$IMAGE_NAME:${IMAGE_TAG-latest}
+docker push $DOCKER_REGISTRY/$DOCKER_USER/$IMAGE_NAME:${IMAGE_TAG-latest}
