c7be063a8b477d1b49a183dbb1afd2ff64b9821e
Abe Hiroshi <abe@x-wave.co.jp>
2015-04-30 01:22:28 +0900

    revert db node

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@
 export default {
   port: (process.env.PORT || 3000),
   mongo: {
-    uri: 'mongodb://' + (process.env[(process.env.DB_NODE || 'DB') + '_PORT_27017_TCP_ADDR'] || 'localhost') + '/working'
+    uri: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/working'
   }
 }
