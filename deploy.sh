24a982749e379397e385116a49594c1b1a8232ec
Abe Hiroshi <abe@x-wave.co.jp>
2015-04-29 23:25:37 +0900

    deploy development

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/sh -e
 
-alias tutum='docker run -e TUTUM_USER=$TUTUM_USER -e TUTUM_APIKEY=$TUTUM_APIKEY -v $(pwd):/app abehiroshi/tutum-cli'
-
-echo "tutum stack update"
-tutum stack update -f /app/tutum.yml ${TUTUM_STACKNAME} || NEW_STACK=true && true
-
-echo "docker push"
+echo "docker push $IMAGE_NAME:${IMAGE_TAG-latest}"
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD -e $DOCKER_EMAIL $DOCKER_REGISTRY
 docker push $IMAGE_NAME:${IMAGE_TAG-latest}
-
-if ${NEW_STACK-false}; then
-  echo "tutum stack up"
-  tutum stack up -n ${TUTUM_STACKNAME} -f /app/tutum.yml 
-fi
